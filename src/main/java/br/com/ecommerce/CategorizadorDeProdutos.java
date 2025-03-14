41acf41f41b99d9cae2e1cb6d1b54639c58d29dd
Fernando pereira <114356049+Fpereiraaraujo@users.noreply.github.com>
2025-03-14 10:08:19 -0300

    feat:manipulando um prompt template

--- src/main/java/br/com/ecommerce/CategorizadorDeProdutos.java
+++ src/main/java/br/com/ecommerce/CategorizadorDeProdutos.java
@@ -7,28 +7,39 @@ import com.theokanning.openai.service.OpenAiService;
 
 import java.time.Duration;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class CategorizadorDeProdutos {
     public static void main(String[] args) {
 
+        var leitor = new Scanner(System.in);
+        System.out.println("Digite as categorias validas");
+        var caterorias = leitor.nextLine();
 
-        var user = "Escova de dentes";
+        System.out.println("Digite o nome do produto");
+        var user = leitor.nextLine();
+
         var system = """
                     Você é um categorizador de produtos e deve responder apenas o nome da categoria do produto informado
                     
                     Escolha uma categoria dentra a lista abaixo:
                     
-                    1. Higiene pessoal
-                    2. Eletronicos
-                    3. Esportes
-                    4. Outros
+                    %s
                     
                     ######Exemplo de uso:
                     
                     Pergunta: Bola de futebol
                     Resposta: Esportes
-                """;
+                    
+                     ###### regras a serem seguidas:
+                     Caso o usuario pergunte algo que nao seja de categorizacao de produtos, voce deve responder que nao pode ajudar pois o seu papel é apenas responder a categoria dos produtos
+                                     
+                """.formatted(caterorias);
 
+        DispararRequisicao(user, system);
+    }
+
+    public static void DispararRequisicao(String user, String system) {
         var chave = System.getenv("OPEN_API_KEY");
 
         var service = new OpenAiService(chave, Duration.ofSeconds(30));
@@ -40,16 +51,13 @@ public class CategorizadorDeProdutos {
                         new ChatMessage(ChatMessageRole.USER.value(), user),
                         new ChatMessage(ChatMessageRole.SYSTEM.value(), system)
                 ))
-                .n(2)
                 .build();
 
         service.createChatCompletion(completionRequest)
                 .getChoices()
-                .forEach(c -> {
-                    System.out.print(c.getMessage().getContent());
-                    System.out.print("\n------------------------\n");
-                });
-
+                .forEach(c -> System.out.print(c.getMessage().getContent()));
 
     }
+
+
 }
